ecc9b574fcfccd00b51040adf79d8bce800027d2
surgeon <surgeon@gateway.(none)>
2012-04-12 20:46:33 -0400

    Created repository.  Much work lies ahead.

---
+++ vectors/rvector.F90
@@ -0,0 +1,152 @@
+! This source file was was generated automatically from a master source
+! code tree, which may or may not be distributed with this code,
+! because it is up to the distributor, and not up to me.
+! If you edit this file (rather than the master source file)
+! your changes will be lost if another pull from the master tree occurs.
+! In case you are wondering why, this approach makes it possible for
+! me to have the same master source code interfaced with different
+! applications (some of which are written in a way that is very far
+! from being object-oriented) at the source level
+!
+!
+      module rvector
+      implicit none
+!
+      type real_vector
+       float, dimension(:), pointer :: r
+       int :: length ! length of the vector
+       int :: last ! index of last element
+       bool :: initialized=.false. ! has the vector been initialized
+      end type real_vector
+!
+      private real_vector_expand
+      int, parameter, private :: expand_incr=500
+!
+      contains
+       subroutine real_vector_init( v )
+       type (real_vector) :: v
+! if (associated(v%r)) deallocate(v%r) ! testing unassigned pointer is an error!
+       allocate(v%r(expand_incr))
+       v%r=0
+       v%length=expand_incr
+       v%last=0
+       v%initialized=.true.
+       end subroutine real_vector_init
+!ccccc
+       subroutine real_vector_done( v )
+       type (real_vector) :: v
+       if (associated(v%r)) deallocate(v%r)
+       v%length=0
+       v%last=0
+       v%initialized=.false.
+       end subroutine real_vector_done
+!ccccc
+       subroutine real_vector_expand( v )
+       type (real_vector) :: v
+       int :: newlength
+       float, dimension(:), allocatable :: p
+!
+       if (.not.v%initialized) then
+        call real_vector_init(v)
+       else
+! assume length is valid
+        newlength=v%length+expand_incr
+        allocate(p(newlength)) ! new memory
+        p(1:v%length)=v%r ! copy old data
+        deallocate(v%r) ! delete old data
+        allocate(v%r(newlength))
+        v%r = p ! copy data
+        deallocate(p)
+        v%length=newlength
+       endif
+       end subroutine real_vector_expand
+!ccccc
+       function real_vector_add( v,i ) ! add a new element to the list (not necessarily unique)
+! and return its index
+       type (real_vector) :: v
+       float :: i
+       int :: j, real_vector_add
+!
+       if (.not.v%initialized) call real_vector_init(v)
+! add element to the list
+       if (v%last.eq.v%length) call real_vector_expand(v)
+       j=v%last+1
+       v%r(j)=i
+       v%last=j
+       real_vector_add=j
+       end function real_vector_add
+!ccccc
+       function real_vector_uadd( v,i ) ! add a UNIQUE new element to the list and return its index
+! if the element already exists, return its index
+       type (real_vector) :: v
+       float :: i
+       int :: j, real_vector_uadd
+!
+       if (.not.v%initialized) call real_vector_init(v)
+       do j=1,v%last
+        if (v%r(j).eq.i) then
+         real_vector_uadd=j
+         return
+        endif
+       enddo
+! add element to the list
+       if (v%last.eq.v%length) call real_vector_expand(v)
+       j=v%last+1
+       v%r(j)=i
+       v%last=j
+       real_vector_uadd=j
+       end function real_vector_uadd
+!ccccc
+       function real_vector_get( v,j ) ! returns v%r(j) if j is valid
+       type (real_vector) :: v
+       float :: i, real_vector_get
+       int :: j
+       if (.not.v%initialized) then
+        i=-1
+       elseif (j.gt.v%last.or.j.le.0) then
+        i=-1
+       else
+        i=v%r(j)
+       endif
+       real_vector_get=i
+       end function real_vector_get
+!ccccc
+       function real_vector_getlast(v) ! returns the last element, if list nonempty
+       type (real_vector) :: v
+       float :: i, real_vector_getlast
+       if (.not.v%initialized) then
+        i=-1
+       elseif (v%last.le.0) then
+        i=-1
+       else
+        i=v%r(v%last)
+       endif
+       real_vector_getlast=i
+       end function real_vector_getlast
+!ccccc
+       function real_vector_getind( v,i ) result(j) ! returns j for the first "v%__DATANAME(j)=i" match
+       type (real_vector) :: v
+       int :: i, j
+       j=-1
+       if (v%initialized) then
+        do j=1,v%last
+         if (v%r(j).eq.i) exit
+        enddo
+        if (j.eq.v%last) then ; if (v%r(j).ne.i) j=-1 ; endif ! not found entry, just ran to end of loop !
+       endif
+       end function real_vector_getind
+!ccccc
+       function real_vector_delete( v,i )
+       type (real_vector) :: v
+       bool :: real_vector_delete
+       int :: i
+       if (i.gt.0.and.i.le.v%last) then ! delete
+        if (i.lt.v%last) v%r(i)=v%r(v%last)
+        v%last=v%last-1
+        real_vector_delete=.true.
+       else ! out of bounds
+        real_vector_delete=.false.
+       endif
+       end function real_vector_delete
+!ccccc
+      end module rvector
---
+++ vectors/rvector_list.F90
@@ -0,0 +1,193 @@
+! This source file was was generated automatically from a master source
+! code tree, which may or may not be distributed with this code,
+! because it is up to the distributor, and not up to me.
+! If you edit this file (rather than the master source file)
+! your changes will be lost if another pull from the master tree occurs.
+! In case you are wondering why, this approach makes it possible for
+! me to have the same master source code interfaced with different
+! applications (some of which are written in a way that is very far
+! from being object-oriented) at the source level
+!
+!
+      module rvector_list
+      use rvector
+      implicit none
+!
+      type real_vlist
+       int, dimension(:), pointer :: i ! int label
+       type (real_vector), dimension(:), pointer :: v ! list of vectors
+       int :: length ! length of the vector
+       int :: last ! index of last element
+       bool :: initialized=.false. ! has the vector been initialized
+      end type real_vlist
+!
+      private real_vlist_expand
+      private real_vlist_add
+      int, parameter, private :: expand_incr=1
+!
+      contains
+
+       subroutine real_vlist_init( vl, length )
+       int, optional :: length
+       int :: i, llength
+       type (real_vlist) :: vl
+! if (associated(vl%v)) then ... ! testing unassigned pointer is an error in F90 !
+       if (present(length)) then ; llength=length;
+       else ; llength=expand_incr ; endif
+       allocate(vl%v(llength))
+       allocate(vl%i(llength))
+       vl%length=llength
+       vl%last=0
+! allocate new vectors and point to them
+       do i=1,vl%length ; call real_vector_init(vl%v(i)) ; enddo
+       vl%i=0
+       vl%initialized=.true.
+       end subroutine real_vlist_init
+!
+       subroutine real_vlist_done( vl )
+       int :: i
+       type (real_vlist) :: vl
+       if (associated(vl%v)) then
+        do i=1,vl%length ; call real_vector_done(vl%v(i)) ; enddo
+        deallocate(vl%v)
+        vl%length=0
+        vl%last=0
+       endif
+       if (associated(vl%i)) deallocate(vl%i)
+       vl%initialized=.false.
+       end subroutine real_vlist_done
+!
+       subroutine real_vlist_expand( vl )
+       type (real_vlist) :: vl
+       int :: newlength, i
+!
+       type (real_vlist) :: wl ! temporary list
+!
+       if (.not.vl%initialized) then
+        call real_vlist_init(vl)
+       else
+        newlength=vl%length+expand_incr
+!
+        allocate(wl%i(newlength)) ! new memory for labels
+        wl%i(1:vl%length)=vl%i ! copy old data
+        deallocate(vl%i) ! delete old data
+        allocate(vl%i(newlength))
+        vl%i = wl%i ! copy data
+!
+        allocate(wl%v(vl%length))
+        do i=1,vl%length ; wl%v(i)=vl%v(i) ; enddo ! do not quite know why this should work in F90, but it seems to ...
+        deallocate(vl%v) ! delete old data
+        allocate(vl%v(newlength))
+        do i=1,vl%length ; vl%v(i)=wl%v(i) ; enddo ! copy existing vectors
+        do i=vl%length+1,newlength;call real_vector_init(vl%v(i));enddo ! allocate space for additional vectors
+        deallocate(wl%v)
+        vl%length=newlength
+       endif
+       end subroutine real_vlist_expand
+!
+       function real_vlist_add( vl, i, j ) ! add a new list labeled 'i' ; then add element 'j' to it & return index of label i
+       type (real_vlist) :: vl
+       int :: i, k, l, real_vlist_add
+       float, optional :: j
+!
+       if (.not.vl%initialized) call real_vlist_init(vl)
+       if (vl%last.eq.vl%length) call real_vlist_expand(vl)
+       k=vl%last+1
+       vl%i(k)=i ! new list label
+       if (present(j)) l=real_vector_add(vl%v(k),j) ! add element to the list
+
+       vl%last=k
+       real_vlist_add=k
+       end function real_vlist_add
+!
+       function real_vlist_uadd( vl, i, j ) ! add a new element to the list and return the index of the list
+       type (real_vlist) :: vl
+       int :: i, k, l, real_vlist_uadd
+       float, optional :: j
+!
+       if (.not.vl%initialized) call real_vlist_init(vl)
+       do k=1,vl%last
+        if (vl%i(k).eq.i) then
+         real_vlist_uadd=k
+         if (present(j)) l=real_vector_add(vl%v(k),j)
+         return
+        endif
+       enddo
+! if we are here, that means a matching entry was not found; we can safely call regular add (not unique)
+       if (present(j)) then
+        real_vlist_uadd=real_vlist_add( vl, i, j )
+       else
+        real_vlist_uadd=real_vlist_add( vl, i )
+       endif
+!
+       end function real_vlist_uadd
+!
+       function real_vlist_uaddu( vl, i, j ) ! add a UNIQUE new element to the list and return the index of the list
+       type (real_vlist) :: vl
+       int :: i, k, l, real_vlist_uaddu
+       float, optional :: j
+!
+       if (.not.vl%initialized) call real_vlist_init(vl)
+       do k=1,vl%last
+        if (vl%i(k).eq.i) then
+         real_vlist_uaddu=k
+         if (present(j)) l=real_vector_uadd(vl%v(k),j)
+         return
+        endif
+       enddo
+! if we are here, that means a matching entry was not found; we can safely call regular add
+       if (present(j)) then
+        real_vlist_uaddu=real_vlist_add( vl, i, j )
+       else
+        real_vlist_uaddu=real_vlist_add( vl, i )
+       endif
+!
+       end function real_vlist_uaddu
+!
+       function real_vlist_get( vl, i ) ! if i is found, returns the corresponding list
+       type (real_vlist) :: vl
+       int :: i, k
+       float, pointer :: real_vlist_get(:)
+       do k=1,vl%last
+         if (vl%i(k).eq.i) then
+          allocate(real_vlist_get(vl%v(k)%last))
+          real_vlist_get=vl%v(k)%r(1:vl%v(k)%last)
+          return
+         endif
+       enddo
+       end function real_vlist_get
+!
+       function real_vlist_getind( vl,i ) result(j) ! returns j for the first "vl%i(j)=i" match
+       type (real_vlist) :: vl
+       int :: i, j
+       j=-1
+       if (vl%initialized) then
+        do j=1,vl%last
+         if (vl%i(j).eq.i) exit
+        enddo
+        if (j.eq.vl%last) then ; if (vl%i(j).ne.i) j=-1 ; endif ! not found entry, just ran to end of loop !
+       endif
+       end function real_vlist_getind
+!
+       function real_vlist_delete( vl,i ) ! delete list that corresponds to the tag 'i'
+       type (real_vlist) :: vl
+       bool :: real_vlist_delete
+       int :: i, k, l
+       real_vlist_delete=.false.
+       do k=1,vl%last
+         if (vl%i(k).eq.i) then
+          l=vl%last
+          vl%i(k)=vl%i(l)
+          call real_vector_done(vl%v(k))
+          vl%v(k)=vl%v(l)
+          vl%last=vl%last-1
+! nullify pointer at position l; otherwise it will hang when we deallocate position i
+          nullify(vl%v(l)%r)
+          real_vlist_delete=.true.
+          exit
+         endif
+       enddo
+!
+       end function real_vlist_delete
+!
+      end module rvector_list
