4b864f46e1198d799bde4b0fa2a97e9b5f631e43
Victor <ovchinnv@georgetown.edu>
2012-10-28 15:34:39 -0400

    Bugfixes in ftsm.ftn and in multicom (output error with large numbers of nodes)

---
+++ string/ftsm/ftsm_var.F90
@@ -0,0 +1,149 @@
+/*#define __WRN(__WHO,__MSG) write(0,*) 'WARNING FROM: ',__WHO,': ',__MSG*/
+/*#define __PRINT(__MSG) write(0,'(A)') __MSG*/
+/*#define __PRINT(__MSG) call plainmessage(__MSG)*/
+/*#define __PRINTL(__MSG,__LEVEL) call plainmessage(__MSG,__LEVEL)*/
+/*COORDINATES AND MASSES:*/
+/*#define __INDX(__STR, __STRLEN, __TEST, __TESTLEN)  index(__STR(1:min(__STRLEN,len(__STR))),__TEST(1:min(__TESTLEN,len(__TEST))))*/
+/*
+#ifdef __IMPNONE
+#undef __IMPNONE
+#endif
+#define __IMPNONE
+*/
+! **********************************************************************!
+! This source file was was generated automatically from a master source !
+! code tree, which may not be distributed with this code if the !
+! distributor has a proprietary compilation procedure (e.g. CHARMM) !
+! If you edit this file (rather than the master source file) !
+! your changes will be lost if another pull from the master tree occurs.!
+! In case you are wondering why, this approach makes it possible for !
+! me to have the same master source code interfaced with different !
+! applications (some of which are written in a way that is quite far !
+! from being object-oriented) at the source level. !
+! **********************************************************************!
+!
+! FTSM_VAR.MOD
+!
+! VARIABLES FOR THE FINITE TEMPERATURE STRING METHOD
+!**CHARMM_ONLY**!##IF STRINGM
+!
+      module ftsm_var
+      !**CHARMM_ONLY**! __DEP_KINDS
+!
+! note that SMCV and FTSM should not be used simultaneously
+! see module smcv_common for the description of the variables below
+!
+      use sm_var, only: &
+     & nstring, mestring, &
+     & repa_initialized, smcv_initialized, &
+     & linear, spline, bspline, dst, linear_exact, interp_method, &
+     & iterations, def, &
+     & dst_cutoff, &
+     & ds, curv, & ! arclength and curvature
+     & stat_iteration_counter, &
+     & output_rmsd0, &
+     & output_arclength, &
+     & output_curvature, &
+     & output_fe, &
+     & output_forces, &
+     & output_rex_map, &
+     & output_rex_log, &
+!
+     & stat_initialized, &
+!
+     & forces_fname, rex_fname, &
+     & rmsd0_fname, s_fname, fe_fname, c_fname, &
+!
+     & rmsd0_funit, s_funit, &
+     & fe_funit, c_funit, &
+     & forces_funit, rex_funit, &
+!
+     & rform, sform, feform, cform, fform, rxlform, &
+!
+     & forces_flen, rmsd0_flen, s_flen, fe_flen, c_flen, rex_flen
+!
+      use sm_config, only: &
+     & ftsm_on, repa_on, restrained_on, &
+     & stat_on, &
+     & string_noprint, restraint_force_on, &
+     & calc_bestfit_grad_para, &
+     & evolve_freq, stat_freq, &
+     & restrained_eq_steps, restrained_eq0, evolve_nskip, &
+     & olditeration, &
+     & evolve_aver_on, & ! in this context, whether the evolution corresponds to averaging the simulation structure
+! ! when false, evolve_expo_on is used, which implies a fixed exponential filter width
+     & evolve_expo_on, &
+     & evolve_expo_mem, &
+     & finite_difference_d, &
+     & parallel_tolerance, &
+     & repl_x_on, &
+     & repl_x_freq, &
+     & rextime_offset
+!
+       real*8, allocatable, save :: fe(:), feav(:) ! free energy arrays
+       real*8, save :: avforce(2) ! average parallel (1) and (2) perpendicular force;
+       real*8, save :: repl_x_temp ! temperature for replica exchange
+       integer, save :: num_evolve_samples=0 ! number of samples in the averaged image
+       integer, save :: max_evolve_samples=0 ! maximum number of allowed samples before wraparound (if > 0)
+       integer, save :: num_fe_samples=0 ! number of f.e. curves in average
+       integer, save :: num_force_samples=0 ! number of force samples
+       logical, save :: ftsm_initialized=.false.
+       logical, save :: evolve_ftsm_on=.false., & ! is string evolution on?
+     & update_on=.false. ! image updating on?
+       logical, save :: output_centers=.false. ! center of the transition tube (these are coordinate files)
+       logical, save :: qdiffrot=.false. ! whether the orientation atoms are different from forcing atoms, in which case store them separately
+       logical, save :: qorient=.false. ! whether images are to be oriented in the bestfit sense
+       logical, save :: proj_on=.false. ! whether the string evolves only along the direction perpendicular to itself
+       character(len=80), save :: centers_fname=''
+       integer, save :: centers_funit=-1, centers_flen=0
+       character(len=80), save :: cenform
+!
+       integer, save :: norient=0, nforced=0, & ! orientation and forcing atoms
+     & nany=0, nboth=0 ! any atoms, overlapping atoms
+       integer, save :: update_freq=0 ! frequency for performing image update
+!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! coordinate variables below
+       real*8, save, pointer :: &
+     & r_f(:,:,:), &! forcing
+     & r_o(:,:,:) ! orientation
+       real*8, save, pointer :: rcom(:,:)
+! weights
+       real*8, save, pointer :: orientWeights(:), forcedWeights(:)
+! indices
+       integer, save, pointer :: iatom_o(:), iatom_f(:), iatom_a(:)
+       integer, save, pointer :: iatom_both(:,:) ! index pairs that correspond to the same PSF atoms
+! define indices into r array:
+       integer, parameter, public :: left=1, & !
+     & center=2, & !
+     & right=3, & !
+     & left_old=4, & ! old structures are saved to provide smooth transitions after evolution
+     & center_old=5, & !
+     & right_old=6, & !
+     & center_new=7, & !
+     & ref=8, & !
+     & rave=9, & !
+     & dummy=10, & !
+     & instant=11, & !
+     & vpar=12, & !
+     & vperp=13, & !
+     & fpar=14, & !
+     & fperp=15, & !
+     & left_rot=16, & !
+     & center_rot=17, & !
+     & right_rot=18, & !
+     & left_cur=19, &
+     & center_cur=20, &
+     & right_cur=21
+!
+       real*8, save :: krms=0d0, kpara=0d0, kperp=0d0, &
+     & dpar0=0.5d0,dperp0=0d0,drms0=0d0,dpar,dperp,drms
+!
+       integer, parameter, public :: num_sets=21 ! num of parameters above
+       integer, save :: MPI_RTMD_TYPE, MPI_RTMD_TYPE_
+!
+       character(len=8), parameter, public :: real_format='(E23.15)'
+       character(len=5), parameter, public :: int_format='(I10)'
+!
+      end module ftsm_var
+!
+!**CHARMM_ONLY**!##ENDIF
---
+++ string/sm_config.F90
@@ -0,0 +1,93 @@
+/*#define __WRN(__WHO,__MSG) write(0,*) 'WARNING FROM: ',__WHO,': ',__MSG*/
+/*#define __PRINT(__MSG) write(0,'(A)') __MSG*/
+/*#define __PRINT(__MSG) call plainmessage(__MSG)*/
+/*#define __PRINTL(__MSG,__LEVEL) call plainmessage(__MSG,__LEVEL)*/
+/*COORDINATES AND MASSES:*/
+/*#define __INDX(__STR, __STRLEN, __TEST, __TESTLEN)  index(__STR(1:min(__STRLEN,len(__STR))),__TEST(1:min(__TESTLEN,len(__TEST))))*/
+/*
+#ifdef __IMPNONE
+#undef __IMPNONE
+#endif
+#define __IMPNONE
+*/
+! **********************************************************************!
+! This source file was was generated automatically from a master source !
+! code tree, which may not be distributed with this code if the !
+! distributor has a proprietary compilation procedure (e.g. CHARMM) !
+! If you edit this file (rather than the master source file) !
+! your changes will be lost if another pull from the master tree occurs.!
+! In case you are wondering why, this approach makes it possible for !
+! me to have the same master source code interfaced with different !
+! applications (some of which are written in a way that is quite far !
+! from being object-oriented) at the source level. !
+! **********************************************************************!
+!
+! SM_CONFIG.MOD
+!
+! MODULE THAT CONTAINS CONFIGURATION OPTIONS FOR SMCV, FTSM AND SM0K
+!**CHARMM_ONLY**!##IF STRINGM
+!
+      module sm_config
+!
+      !**CHARMM_ONLY**! __DEP_KINDS
+!
+!ccccc flags
+      logical :: &
+     & smcv_on=.false., repa_on=.false., restrained_on=.false., &
+     & stat_on=.false., evolve_cv_on=.false., unrestrained_on=.false., &
+     & voronoi_hist_on=.false., voronoi_allow_cross=.false., &
+     & evolve_expo_on=.false., &
+     & evolve_smooth_on=.false., evolve_aver_on=.false., &
+     & evolve_bd_on=.false., &
+     & voronoi_wrong_cell=.false., & ! this flag is on if the system belongs to a wrong cell for 2 consecutive steps.
+     & string_noprint=.false., restraint_force_on=.false., &
+     & planar_on=.false., repl_x_on=.false., compute_whereami=.false., &
+     & ftsm_on=.false., confcons_on=.false., chirality_on=.false.
+      integer :: evolve_freq=0, repa_freq=0, stat_freq=0, hist_freq=0, &
+     & restrained_eq_steps=0, restrained_eq0=0, evolve_nskip=0, &
+     & evolve_smooth_d=10, unrestrained_eq_steps=0, unrestrained_eq0=0, &
+     & num_ave_cv_samples=0, vtime_offset=0, olditeration=0, & ! timestep offset for voronoi calculations
+     & repl_x_freq=0, rextime_offset=0, voronoi_update_freq=0, &
+     & voronoi_nocross_ini=0, confcons_freq=0, chirality_freq=0
+!
+      real*8 :: evolve_step=0.0d0, evolve_expo_mem=1.00d0, &
+     & evolve_bd_T=0d0
+!
+! cv index arrays (for parallelization)
+! integer, allocatable, dimension(:) ::
+! & cv_send_displ, cv_send_count,
+! & fr_send_displ, fr_send_count,
+! & qt_send_displ, qt_send_count,
+! & imap_displ, imap_count
+      integer*4, allocatable, dimension(:) :: &
+     & cv_send_displ, cv_send_count, &
+     & fr_send_displ, fr_send_count, &
+     & qt_send_displ, qt_send_count, &
+     & imap_displ, imap_count
+! MPI derived types
+! integer :: MPI_CV_TYPE2_, MPI_CV_TYPE3_, MPI_GRAD_TYPE_,
+! & MPI_CV_TYPE3I_
+! integer :: MPI_CV_TYPE2 , MPI_CV_TYPE3 , MPI_GRAD_TYPE,
+! & MPI_CV_TYPE3I
+      integer*4 :: MPI_CV_TYPE2_, MPI_CV_TYPE3_, MPI_GRAD_TYPE_, &
+     & MPI_CV_TYPE3I_
+      integer*4 :: MPI_CV_TYPE2 , MPI_CV_TYPE3 , MPI_GRAD_TYPE, &
+     & MPI_CV_TYPE3I
+! flags that control when to use parallelization
+      logical :: calc_cv_para=.false., & ! should CVs be calculated in parallel?
+     & calc_fr_para=.false., & ! should frames be calculated in parallel?
+     & calc_qt_para=.false., & ! should quaternions be calculated in parallel?
+     & calc_voronoi_para=.true., & ! should the tessellation metric test be done in parallel?
+     & calc_Mtensor_para=.false., &! should the calculation of the M tensor be done in parallel?
+     & calc_Mtensor_fast=.true., &! should the calculation of the M tensor use the fast algorithm for sparse matrices?
+     & inverse_LU=.true., & ! use LU decomposition for solving Ax=b (and for A inverse)
+     & calc_bestfit_grad_para=.false. ! should the calculation of rotation gradients be in parallel?
+! finite difference perturbation
+      real*8 :: finite_difference_d=0.000001d0
+      real*8 :: parallel_tolerance=1.0d-13
+!
+      integer, parameter :: sizeofreal=8
+!
+!
+      end module sm_config
+!**CHARMM_ONLY**!##ENDIF
