267c2bf274a5580c020bfb06269061ff464e2b68
Victor <ovchinnv@georgetown.edu>
2014-01-26 21:34:52 -0500

    working on diagonalization

---
+++ dynamol/test/pca.m
@@ -0,0 +1,31 @@
+% diagonalize covariance matrix
+%
+
+delf='int32';
+intf='int32';
+ff='double';
+
+fname='rab11a_cov.dat';
+
+fid=fopen(fname,'r');
+
+d=fread(fid,1,delf);
+
+n=fread(fid,2,intf);
+nx=n(1); ny=n(2);
+
+d=fread(fid,2,delf);
+
+a=fread(fid,[nx ny],ff); pcolor(a); shading interp; colorbar;
+
+% 
+mass=12.0110;
+a=a/mass/mass;
+
+save 'a.dat' -ascii a
+
+return
+
+%diagonalize
+[ev,e]=eig(a);
+e=diag(e)
