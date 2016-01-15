67ebae0bd8b47a18d7c17a0f4b2937f0b7a28b0c
Victor <ovchinnv@gmx.com>
2016-01-14 21:05:17 -0500

    further progress onACEMD plugin : actually calling smcv module; next : add cv functionality

--- string/smcv/acemd/plugin-smcv.c
+++ string/smcv/acemd/plugin-smcv.c
@@ -12,38 +12,73 @@
 
 aceplug_err_t aceplug_init(struct aceplug_sim_t *s, int argc, char **argkey, char **argval) {
  __CCHAR *inputfile = NULL, *logfile = NULL;
- const __CCHAR* deflogfile = "smcv.log" ;
+ __CINT ilen=0, llen=0;
+ __CCHAR* deflogfile = "smcv.log" ;
+//
+ printf("# SMCV PLUGIN: Initializing ...\n");
+//
  for (int i=0; i<argc; i++){
   if ( !(strcmp(argkey[i], "input")) || !(strcmp(argkey[i], "inputfile"))) {
    inputfile=argval[i];
   } else if ( !(strcmp(argkey[i], "output")) || !(strcmp(argkey[i], "outputfile")) || !(strcmp(argkey[i], "log")) || !(strcmp(argkey[i], "logfile"))) {
-   logfile=argval[i]; 
+   logfile=argval[i];
   }
  } // for
  if (inputfile == NULL) {
-  printf("#SMCV PLUGIN: input file not specified (syntax : input <inputfile> log <logfile>)\n");
+  printf("# SMCV PLUGIN: input file not specified (syntax : input <inputfile> log <logfile>)\n");
   exit(1);
+ } else {
+  printf("# SMCV PLUGIN: input file is '"); 
+  printf(inputfile);
+  printf("'\n");
  }
 //
  if (logfile == NULL) {
-  printf("#SMCV PLUGIN: log file not specified (syntax : input <inputfile> log <logfile>); uisng ");
-  printf(deflogfile);
-  printf("\n");
+  printf("# SMCV PLUGIN: log file not specified (syntax : input <inputfile> log <logfile>); using '");
   logfile=deflogfile;
+  printf(logfile);
+  printf("'\n");
+ } else {
+  printf("# SMCV PLUGIN: log file is '"); 
+  printf(logfile);
+  printf("'\n");
  }
+
 
  __CINT natoms=0;
  __CFLOAT *m=NULL, *q=NULL;
 
  natoms = s->natoms ;
+//
  m = (__CFLOAT *) calloc(natoms, sizeof(__CFLOAT));
  q = (__CFLOAT *) calloc(natoms, sizeof(__CFLOAT));
  for (int i=0; i<natoms; i++){
   m[i]=s->mass[i];
   q[i]=s->charge[i];
  }
-
- smcv_init_from_acemd(natoms, m, q, inputfile, logfile) ;
 //
- return 0;
+ ilen=strlen(inputfile);
+ llen=strlen(logfile);
+ smcv_init_from_acemd(natoms, m, q, inputfile, ilen, logfile, llen) ;
+//
+ return ACEPLUG_OK;
 }
+//==========================================================
+aceplug_err_t aceplug_calcforces(struct aceplug_sim_t *s) {
+
+
+ return ACEPLUG_OK;
+}
+//==========================================================
+aceplug_err_t aceplug_endstep(struct aceplug_sim_t *s) {
+
+
+ return ACEPLUG_OK;
+}
+//==========================================================
+aceplug_err_t aceplug_terminate(struct aceplug_sim_t *s) {
+
+ smcv_done_from_acemd();
+ return ACEPLUG_OK;
+}
+
--- string/smcv/acemd/smcv.h
+++ string/smcv/acemd/smcv.h
@@ -1,2 +1,3 @@
 /* C prototypes */
-extern void smcv_init_from_acemd (const integer, const double *,const double *, const char * , const char *);
+// C, not Fortran!
+extern void smcv_init_from_acemd (const int, const double *,const double *, const char * , const int, const char *, const int);
