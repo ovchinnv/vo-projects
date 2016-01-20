21070f84d1be742cae6c6fb61c0214d35487399a
Victor <ovchinnv@gmx.com>
2016-01-19 20:46:45 -0500

    cleaned compilation environment a little , so that can compile acemd w/o conflicts; will try to make sure charmm compilation is unmodified

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 DIRS=driver state bestfit constants dynamol erf files lu multicom multidiag output parselist parser random string timer vectors \
- confcons enm continua pnm
+ confcons enm continua pnm mpi_stub
 DISTRIB=include obj lib
 
 
--- string/smcv/acemd/smcv.h
+++
@@ -1,5 +0,0 @@
-/* C prototypes */
-// C, not Fortran!
-extern int smcv_init_from_acemd (const int, const double *,const double *, const char * , const int, const char *, const int, int **);
-extern void smcv_done_from_acemd ();
-extern int smcv_dyna_from_acemd ( const long int, const double *, float *, double *, int ** );
