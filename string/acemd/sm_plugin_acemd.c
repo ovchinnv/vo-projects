408a13d818e2a0d54e7f863e2afc0f102ce4fa41
Victor <ovchinnv@gmx.com>
2016-04-07 15:31:57 -0400

    first compilation of acemd ftsm/smcv combined plugin -- test next

--- string/acemd/sm_plugin_acemd.c
+++ string/acemd/sm_plugin_acemd.c
@@ -1,4 +1,4 @@
-// SMCV plugin for ACEMD
+// String Method Plugin for ACEMD; limited exposure of SMCV and FTSM functionality
 // C, not Fortran!
 #ifdef int
 #undef int
@@ -7,7 +7,7 @@
 #include "tcl.h"
 #include "aceplug.h"
 #include "string.h"
-#include "smcv.h"
+#include "sm.h"
 #include <stdlib.h>
 
 // structore for plugin private data :
@@ -18,39 +18,55 @@ typedef struct {
 } pdata ;
 
 aceplug_err_t aceplug_init(struct aceplug_sim_t *s, int argc, char **argkey, char **argval) {
- __CCHAR *inputfile = NULL, *logfile = NULL;
- __CINT ilen=0, llen=0, ierr=0;
- __CCHAR *deflogfile = "smcv.log" ;
+ __CCHAR *inputfile = NULL, *logfile = NULL, *smtype = NULL;
+ __CINT ilen=0, llen=0, smlen=0, ierr=0;
+ __CCHAR *deflogfile = "struna.log" ;
+ __CCHAR *defaultflavor = "SMCV";
 // for plugin private data :
  pdata *private_data = malloc(sizeof(pdata)) ;
 //
- printf("# SMCV PLUGIN: Initializing ...\n");
+ printf("# STRUNA PLUGIN: Initializing ...\n");
 //
  int i;
  for (i=0; i<argc; i++){
-  if ( !(strcmp(argkey[i], "input")) || !(strcmp(argkey[i], "inputfile"))) {
+  if ( !(strcmp(argkey[i], "input")) || !(strcmp(argkey[i], "inputfile")) || !(strcmp(argkey[i], "INPUT")) || !(strcmp(argkey[i], "INPUTFILE")) ) {
    inputfile=argval[i];
-  } else if ( !(strcmp(argkey[i], "output")) || !(strcmp(argkey[i], "outputfile")) || !(strcmp(argkey[i], "log")) || !(strcmp(argkey[i], "logfile"))) {
+  } else if ( !(strcmp(argkey[i], "output")) || !(strcmp(argkey[i], "outputfile")) || !(strcmp(argkey[i], "log")) || !(strcmp(argkey[i], "logfile")) ||\
+              !(strcmp(argkey[i], "OUTPUT")) || !(strcmp(argkey[i], "OUTPUTFILE")) || !(strcmp(argkey[i], "LOG")) || !(strcmp(argkey[i], "LOGFILE")) ) {
    logfile=argval[i];
+  } else if ( !(strcmp(argkey[i], "flavor")) || !(strcmp(argkey[i], "FLAVOR")) || !(strcmp(argkey[i], "method")) || !(strcmp(argkey[i], "METHOD")) ||\
+              !(strcmp(argkey[i], "type"))   || !(strcmp(argkey[i], "TYPE")) || !(strcmp(argkey[i], "alg")) || !(strcmp(argkey[i], "ALG")) ) {
+   smtype=argval[i];
   }
  } // for
  if (inputfile == NULL) {
-  printf("# SMCV PLUGIN: input file not specified (syntax : input <inputfile> log <logfile>)\n");
+  printf("# STRUNA PLUGIN: input file not specified (syntax : input <inputfile>)\n");
   exit(1);
  } else {
-  printf("# SMCV PLUGIN: input file is '"); 
+  printf("# STRUNA PLUGIN: input file is '"); 
   printf(inputfile);
   printf("'\n");
  }
 //
  if (logfile == NULL) {
-  printf("# SMCV PLUGIN: log file not specified (syntax : input <inputfile> log <logfile>); using '");
+  printf("# STRUNA PLUGIN: log file not specified (syntax : log <logfile>); using '");
   logfile=deflogfile;
   printf(logfile);
   printf("'\n");
  } else {
-  printf("# SMCV PLUGIN: log file is '"); 
+  printf("# STRUNA PLUGIN: log file is '"); 
   printf(logfile);
+  printf("'\n");
+ }
+//
+ if (smtype == NULL) {
+  printf("# STRUNA PLUGIN: method not specified (syntax : method <SMCV|FTSM>); using '");
+  smtype=defaultflavor;
+  printf(smtype);
+  printf("'\n");
+ } else {
+  printf("# STRUNA PLUGIN: log file is '"); 
+  printf(smtype);
   printf("'\n");
  }
 
@@ -73,7 +89,7 @@ aceplug_err_t aceplug_init(struct aceplug_sim_t *s, int argc, char **argkey, cha
 //
  ilen=strlen(inputfile);
  llen=strlen(logfile);
- ierr=smcv_init_from_acemd(natoms, m, q, inputfile, ilen, logfile, llen, &(private_data->atomlist));
+ ierr=sm_init_from_acemd(natoms, m, q, inputfile, ilen, logfile, llen, &(private_data->atomlist), smtype, smlen);
  free(m);
  free(q);
 //
@@ -93,7 +109,7 @@ aceplug_err_t aceplug_calcforces(struct aceplug_sim_t *s) {
  __CINT *aptr ;
  __CFLOAT *rptr;
  float *fptr;
- __CFLOAT smcv_energy;
+ __CFLOAT sm_energy;
  long int iteration =s->step ;
 //
 //
@@ -107,7 +123,7 @@ aceplug_err_t aceplug_calcforces(struct aceplug_sim_t *s) {
    *(rptr++) = s -> pos[i].z;
   }
 // compute plugin forces
-  ierr=smcv_dyna_from_acemd(iteration, r, fr, &smcv_energy, &atomlist); // might return valid atomlist
+  ierr=sm_dyna_from_acemd(iteration, r, fr, &sm_energy, &atomlist); // might return valid atomlist
   private_data->atomlist=atomlist ;
 // apply plugin forces
   if (atomlist!=NULL) { // atom indices provided; store them as private data and use for adding forces
@@ -134,7 +150,7 @@ aceplug_err_t aceplug_calcforces(struct aceplug_sim_t *s) {
    *(rptr)   = s -> pos[j].z;
   }
 //
-  ierr=smcv_dyna_from_acemd(iteration, r, fr, &smcv_energy, &atomlist); // atomlist should not be modified in this call
+  ierr=sm_dyna_from_acemd(iteration, r, fr, &sm_energy, &atomlist); // atomlist should not be modified in this call
 //
   for (aptr=atomlist+1 ; aptr<atomlist + 1 + (*atomlist) ; aptr++) { // iterate until atomlist points to the last index
    j=*aptr - 1;
@@ -147,9 +163,9 @@ aceplug_err_t aceplug_calcforces(struct aceplug_sim_t *s) {
  } // atomlist == NULL
 //
  if ( s-> plugin_update_forces() ) { return ACEPLUG_ERR;}
- if ( s-> plugin_add_energy(ENERGY_EXTERNAL, smcv_energy) ) { return ACEPLUG_ERR;}
+ if ( s-> plugin_add_energy(ENERGY_EXTERNAL, sm_energy) ) { return ACEPLUG_ERR;}
 // we know that energy is computed, but it does not seem to make into the ACEMD energy output
-//printf("%12.5f\n", smcv_energy);
+//printf("%12.5f\n", sm_energy);
 //
  return (ierr>0) ? ACEPLUG_ERR : ACEPLUG_OK ;
 }
@@ -160,8 +176,8 @@ aceplug_err_t aceplug_endstep(struct aceplug_sim_t *s) {
 //==========================================================
 aceplug_err_t aceplug_terminate(struct aceplug_sim_t *s) {
  pdata *private_data = s->privdata ;
- printf("# SMCV PLUGIN: Finalizing...\n");
- smcv_done_from_acemd();
+ printf("# STRUNA PLUGIN: Finalizing...\n");
+ sm_done_from_acemd();
  free(private_data->r);
  free(private_data->fr);
  private_data->atomlist=NULL ; // this memory is managed by FORTRAN
