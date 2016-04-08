9d592d258df61946fb56851c8b15f4ade6a6ffb3
Victor <ovchinnv@gmx.com>
2016-04-07 20:48:09 -0400

    further debugging mods to run ftsm plugin ; now need to figure out how to read individual ftsm coords (.cor option requires topology)

---
+++ string/acemd/test/diala/fts2/postpro/diheall.m
@@ -0,0 +1,79 @@
+close;
+
+map=load('adia.dat');
+
+phi=map(:,1);
+psi=map(:,2);
+ene=map(:,3);
+
+n=sqrt(length(phi));
+phi=reshape(phi,n,n);
+psi=reshape(psi,n,n);
+ene=reshape(ene,n,n);
+
+pcolor(phi, psi, ene);shading interp;%colorbar;
+hold on;colormap pink
+
+if 1
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% now can overlay paths
+name='dihe_dcd16_1.dat';
+
+%
+d=load(name);
+%
+skip=5000; % number of data-points in a single trajectory
+skip=500;
+%
+% reorganize data into a matrix
+id=d(:,1);
+phi=d(:,2);
+psi=d(:,3);
+%
+phi=reshape(phi,skip,[]);
+psi=reshape(psi,skip,[]);
+id=reshape(id,skip,[]);
+%
+styles={'r','g','b','m','y','w'};
+%
+%plot(phi,psi,char(styles(1)))
+%
+[m,nrep]=size(phi)
+
+reps=[1:6, 10:16]
+reps=[2:2:16]
+reps=[1:16]
+%
+step=1;
+j=0;
+for k=1:length(reps)
+ i=reps(k);
+ j=j+1;
+ plot(phi(1:step:end,i), psi(1:step:end,i),[char(styles( mod(j-1,length(styles))+1 )),'.'],'MarkerSize',5);
+end
+
+end % traj
+
+% overlay string
+name='../string.dihe';
+%name='../data/path.dihe';
+d=load(name);
+id=d(:,1);
+phi=d(:,2);
+psi=d(:,3);
+
+% average 
+%phi=reshape(phi,nrep,[]); phi=mean(phi,2);
+%psi=reshape(psi,nrep,[]); psi=mean(psi,2);
+
+plot(phi, psi,'k.','MarkerSize',25); hold on
+
+
+set(gca,'FontSize',16);
+
+xlabel('\phi', 'FontSize',16);
+ylabel('\psi', 'FontSize',16);
+
+set(gcf, 'paperpositionmode', 'auto');
+print(gcf, '-djpeg100', 'surf1.jpg');
+
+
