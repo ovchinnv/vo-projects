ce229fd736ea8c1a6ce2150a1c08dedfe82945b0
Victor <ovchinnv@gmx.com>
2016-04-26 21:18:29 -0400

    mod to fcombine to take an optional number of samples

--- string/acemd/test/diala/fts2/data/fcombine.m
+++ string/acemd/test/diala/fts2/data/fcombine.m
@@ -4,15 +4,27 @@
 brep=0;
 erep=15;
 
+%nsamples=390 ; % number of force samples to take from file (useful if files do not have an equal number of samples)
+
 basename='force';
 ext='.dat';
 
 for i=brep:erep
  fname=[basename, num2str(i),ext];
  if (i==brep)
-  d=load(fname);
+  dnew=load(fname);
+  if exist('nsamples')
+   d=dnew(1:nsamples,:);
+  else
+   d=dnew;
+  end
  else
-  d=[d load(fname)];
+  dnew=load(fname);
+  if exist('nsamples')
+   d=[d dnew(1:nsamples,:)];
+  else
+   d=[d dnew];
+  end
  end
 end
 
@@ -21,7 +33,7 @@ fname=[basename,ext];
 fout=fopen(fname,'w');
 
 nlines=size(d,1);
-isamp=0
+isamp=0;
 for i=1:2:nlines
  isamp=isamp+1;
  fprintf(fout,'%% %10d\n',isamp) ;
