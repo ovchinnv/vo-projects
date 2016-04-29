3087d36e19434c3801ea2d7e2e5146472736185b
Victor <ovchinnv@gmx.com>
2016-04-29 14:33:42 -0400

    added smcv_resgtraint_energy output to stats (NOTE that this was optional, in the sense that one can already output the energy array using history;) Also made sure that energy is calculated upon any call to _calc (it is cheap to do so) in the cases when fext is provided, this energy will not be useful, eg if planar sampling is on Hopefully this will make rex implementable at script level

--- string/acemd/test/rab11a/m5b-m5a/data/fcombine.m
+++ string/acemd/test/rab11a/m5b-m5a/data/fcombine.m
@@ -4,7 +4,7 @@
 brep=0;
 erep=31;
 
-nsamples=47 ; % number of force samples to take from file (useful if files do not have an equal number of samples)
+nsamples=124 ; % number of force samples to take from file (useful if files do not have an equal number of samples)
 
 basename='force';
 ext='.dat';
