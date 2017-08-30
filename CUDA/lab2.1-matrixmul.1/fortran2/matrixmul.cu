d33d53e6193294ffc6b99b6200d27eec2222972d
Victor <ovchinnv@gmx.com>
2017-08-30 17:06:39 -0400

    working out the details of gpu memory passing and allocation from fortran

--- CUDA/lab2.1-matrixmul.1/fortran2/matrixmul.cu
+++ CUDA/lab2.1-matrixmul.1/fortran2/matrixmul.cu
@@ -15,35 +15,9 @@
     printf("CUDA error: %s, line %d\n", cudaGetErrorString(err), __LINE__);}}
 
 ////////////////////////////////////////////////////////////////////////////////
-extern "C" void matmul_cuda(float *M, float *N, float *P, int my, int mx, int nx) {
+extern "C" void matmul_cuda(float *deviceM, float *deviceN, float *deviceP, int my, int mx, int nx) {
     StopWatchInterface *timer_compute = NULL;
-    StopWatchInterface *timer_memory = NULL;
-    unsigned int mem_size_M, mem_size_N, mem_size_P;
-    float * deviceM = NULL, * deviceN = NULL, * deviceP = NULL;
     int block_size = 8;
-
-    mem_size_M=my*mx*sizeof(float);
-    mem_size_N=mx*nx*sizeof(float);
-    mem_size_P=my*nx*sizeof(float);
-
-    sdkCreateTimer(&timer_memory);
-    sdkStartTimer(&timer_memory);
-
-    printf("  Allocate device memory.\n");
-    checkCudaErrors(cudaMalloc(&deviceM, mem_size_M ));
-    checkCudaErrors(cudaMalloc(&deviceN, mem_size_N ));
-
-    printf("  Copy host memory to device.\n");
-    checkCudaErrors(cudaMemcpy(deviceM, M, mem_size_M, cudaMemcpyHostToDevice ));
-    checkCudaErrors(cudaMemcpy(deviceN, N, mem_size_N, cudaMemcpyHostToDevice ));
-
-    printf("  Allocate device memory for results.\n");
-    checkCudaErrors(cudaMalloc(&deviceP, mem_size_P ));
-
-    // Clear device memory
-    cudaMemset(deviceP, 0, mem_size_P);
-
-    sdkStopTimer(&timer_memory);
 
     printf("  Setup kernel execution parameters.\n");
     dim3 block(block_size, block_size); // thread indexing : there will be block_size ^ 2 threads per block, indexed (threadIdx.x threadIdx.y)
@@ -80,28 +54,12 @@ extern "C" void matmul_cuda(float *M, float *N, float *P, int my, int mx, int nx
     // Copy the results back from the host
     // ===================================================================
 
-    sdkStartTimer(&timer_memory);
-
-    printf("  Copy result from device to host.\n");
-    cudaMemcpy(P, deviceP, mem_size_P, cudaMemcpyDeviceToHost );
-
-    sdkStopTimer(&timer_memory);
-
     // ================================================
     // Show timing information
     // ================================================
 
-    printf("  GPU memory access time: %f (ms)\n",
-        sdkGetTimerValue(&timer_memory));
     printf("  GPU computation time  : %f (ms)\n",
         sdkGetTimerValue(&timer_compute));
-    printf("  GPU processing time   : %f (ms)\n",
-        sdkGetTimerValue(&timer_compute) + sdkGetTimerValue(&timer_memory));
-    sdkDeleteTimer(&timer_memory);
     sdkDeleteTimer(&timer_compute);
-
-    checkCudaErrors(cudaFree(deviceM ));
-    checkCudaErrors(cudaFree(deviceN ));
-    checkCudaErrors(cudaFree(deviceP ));
 
 }
