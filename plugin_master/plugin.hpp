0dc1fe6e0831b8937166451d31f027588e641591
Victor <ovchinnv@gmx.com>
2018-01-17 13:55:08 -0500

    treatment of aceplug.h -- download

--- plugin_master/plugin.hpp
+++
@@ -1,5 +0,0 @@
-/* C prototypes */
-// C, not Fortran!
-extern "C" int master_init_plugin(const int, const double *, const double *, const char *, const int, const char *, const int, int **, const _Bool, const double *);
-extern "C" int master_dyna_plugin(const long int, const double *, double *, float *, const _Bool, double *, int **, const _Bool, const double * );
-extern "C" void master_done_plugin();
