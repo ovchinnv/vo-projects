8aeae5898e58021f699b0b83aeba332bec6dbadb
taly <surgeon@tesla.(none)>
2018-01-25 11:18:51 -0500

    local changes

--- string/plugin/openmm-struna/openmmapi/include/StrunaKernels.h
+++ string/plugin/openmm-struna/openmmapi/include/StrunaKernels.h
@@ -40,6 +40,15 @@
 
 namespace StrunaPlugin {
 
+    static const double Cal2J = 4.184;
+    static const double J2Cal = 1./Cal2J;
+    static const double nm2A = 10;
+    static const double A2nm = 1.0/nm2A;
+    static const double omm2str_e=J2Cal; // energy conversion
+    static const double str2omm_e=Cal2J;
+    static const double omm2str_f=J2Cal/nm2A;   // force conversion
+    static const double str2omm_f=Cal2J/A2nm;
+
 /**
  * This kernel is invoked by StrunaForce to calculate the forces acting on the system and the energy of the system.
  */
@@ -67,14 +76,6 @@ public:
      */
     virtual double execute(OpenMM::ContextImpl& context, bool includeForces, bool includeEnergy) = 0;
 // constants for conversion between OPENMM and STRUNA units :
-    const double Cal2J = 4.184;
-    const double J2Cal = 1./Cal2J;
-    const double nm2A = 10;
-    const double A2nm = 1.0/nm2A;
-    const double omm2str_e=J2Cal; // energy conversion
-    const double str2omm_e=Cal2J;
-    const double omm2str_f=J2Cal/nm2A;   // force conversion
-    const double str2omm_f=Cal2J/A2nm;
 };
 
 } // namespace StrunaPlugin
