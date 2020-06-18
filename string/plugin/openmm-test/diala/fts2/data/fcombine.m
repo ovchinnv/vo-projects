77e266057469947205f013a7fa4681d8167cd73b
Victor <ovchinnv@gmx.com>
2020-06-18 17:27:53 -0400

    fts test for openmm/diala/fts ; mods to compute psf_natom even if psf_initialized=false to allow psf hack

---
+++ string/plugin/openmm-test/diala/fts2/data/fcombine.m
@@ -0,0 +1,45 @@
+% combine multiple force files from serially-run replicas into one file
+%
+
+brep=0;
+erep=15;
+
+%nsamples=390 ; % number of force samples to take from file (useful if files do not have an equal number of samples)
+
+basename='force';
+ext='.dat';
+
+for i=brep:erep
+ fname=[basename, num2str(i),ext];
+ if (i==brep)
+  dnew=load(fname);
+  if exist('nsamples')
+   d=dnew(1:nsamples,:);
+  else
+   d=dnew;
+  end
+ else
+  dnew=load(fname);
+  if exist('nsamples')
+   d=[d dnew(1:nsamples,:)];
+  else
+   d=[d dnew];
+  end
+ end
+end
+
+% write one file
+fname=[basename,ext];
+fout=fopen(fname,'w');
+
+nlines=size(d,1);
+isamp=0;
+for i=1:2:nlines
+ isamp=isamp+1;
+ fprintf(fout,'%% %10d\n',isamp) ;
+ fprintf(fout,[repmat('%15.10f ',[1,erep-brep+1]),'\n'],d(i,:));
+ fprintf(fout,[repmat('%15.10f ',[1,erep-brep+1]),'\n'],d(i+1,:));
+
+end
+
+fclose(fout)
---
+++ string/plugin/openmm-test/diala/fts2/postpro/diheall.m
@@ -0,0 +1,87 @@
+
+if exist('OCTAVE_VERSION','builtin')
+ clear graphics_toolkit;
+ graphics_toolkit('gnuplot')
+% graphics_toolkit('fltk')
+end
+
+close;
+
+map=load('adia.dat');
+
+phi=map(:,1);
+psi=map(:,2);
+ene=map(:,3);
+
+n=sqrt(length(phi));
+phi=reshape(phi,n,n);
+psi=reshape(psi,n,n);
+ene=reshape(ene,n,n);
+
+pcolor(phi, psi, ene);shading interp;%colorbar;
+hold on;colormap pink
+
+if 1
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% now can overlay paths
+name='dihe_dcd16_1.dat';
+
+%
+d=load(name);
+%
+skip=5000; % number of data-points in a single trajectory
+skip=49;
+%
+% reorganize data into a matrix
+id=d(:,1);
+phi=d(:,2);
+psi=d(:,3);
+%
+phi=reshape(phi,skip,[]);
+psi=reshape(psi,skip,[]);
+id=reshape(id,skip,[]);
+%
+styles={'r','g','b','m','y','w'};
+%
+%plot(phi,psi,char(styles(1)))
+%
+[m,nrep]=size(phi)
+
+reps=[1:6, 10:16]
+reps=[2:2:16]
+reps=[1:16]
+%
+step=1;
+j=0;
+for k=1:length(reps)
+ i=reps(k);
+ j=j+1;
+ plot(phi(1:step:end,i), psi(1:step:end,i),[char(styles( mod(j-1,length(styles))+1 )),'.'],'MarkerSize',5);
+end
+
+end % traj
+
+% overlay string
+name='../data/diala22_zts_.dihe'
+
+d=load(name);
+id=d(:,1);
+phi=d(:,2);
+psi=d(:,3);
+
+% average 
+%phi=reshape(phi,nrep,[]); phi=mean(phi,2);
+%psi=reshape(psi,nrep,[]); psi=mean(psi,2);
+
+plot(phi, psi,'k.','MarkerSize',25); hold on
+
+
+set(gca,'FontSize',16);
+
+xlabel('\phi', 'FontSize',16);
+ylabel('\psi', 'FontSize',16);
+
+set(gcf, 'paperpositionmode', 'auto');
+%print(gcf, '-dpdf', 'surf1.pdf');
+print(gcf, '-depsc2', 'surf1.eps');
+
+
